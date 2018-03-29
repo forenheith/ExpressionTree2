85adb3365ac7adbf3a444d6eb02d29590bc996f4
Yauseyeu Raman <raman.yauseyeu@gmail.com>
2018-03-30 00:32:30 +0300

    Add files via upload

---
+++ ExpressionTree2Tests/MappingGeneratorTests.cs
@@ -0,0 +1,22 @@
+﻿using ExpressionTree2;
+using ExpressionTree2.TestCalsses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExpressionTree2Tests
+{
+    [TestClass]
+    public class MappingGeneratorTests
+    {
+        [TestMethod]
+        public void GenerateTest()
+        {
+            var dest = new MappingGenerator().Generate<ClassA, ClassB>();
+            var classA = new ClassA();
+            var mappedClassB = dest(classA);
+
+            Assert.AreEqual(mappedClassB.Name, classA.Name);
+            Assert.AreEqual(mappedClassB.DateTime, classA.DateTime);
+            Assert.AreEqual(mappedClassB.DataSize, classA.DataSize);
+        }
+    }
+}
